58807746f932d18afd090a02f2fb839a3f9a3e68
Jimmy Sihlberg <tfy12jsg@cs.umu.se>
2016-04-11 14:42:18 +0200

    Added Emitter

---
+++ Assets/Emitter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Emitter : MonoBehaviour {
+
+    public Vector3 position = new Vector3(0,0,0);
+    public float rate = 1.0f;
+
+    public GameObject particlePrefab;
+
+    private PE.ParticleSystem ps;
+
+    private float lastEmitted = 0;
+    
+	// Use this for initialization
+	void Start () {
+        ps = new PE.ParticleSystem();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float t = Time.fixedTime;
+        
+        if (t - lastEmitted >= 1/rate)
+        {   
+            /* Add particle to particleSystem */   
+            lastEmitted = t;
+        }
+        
+	}
+}
