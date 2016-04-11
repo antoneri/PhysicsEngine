93f04aeab91d1e302a198f76cb48fa231929e50f
Anton Eriksson <antoneri@gmail.com>
2016-04-11 11:34:20 +0200

    Naive first implementation.

--- Assets/PEParticleSystem.cs
+++ Assets/PEParticleSystem.cs
@@ -1,32 +1,29 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 namespace PE
 {
 	public class ParticleSystem : MonoBehaviour
 	{
-		public int N = 100;
-		Particle[] particles;
-		public double[] x_init = { 0, 1, 0 };
-		public double[] v_init = { 10, 10, 0 };
+		public int N = 1000;
+		public double MAX_AGE = 10;
+		List<Particle> particles;
 
 		// Use this for initialization
 		void Start ()
 		{
-			particles = new Particle[N];
-
-			for (int i = 0; i < N; i++) {
-				particles [i].x = x_init;
-				particles [i].v = v_init;
-				particles [i].f = new double[3] { 0, 0, 0 };
-				particles [i].m = 1e-4;
-			}
+			particles = new List<Particle> ();
 		}
 
 		// Update is called once per frame
 		void Update ()
 		{
+			particles.RemoveAll (p => p.age >= MAX_AGE);
+		}
 
+		void AddParticle (Particle p)
+		{
+			particles.Add (p);
 		}
 	}
 }
