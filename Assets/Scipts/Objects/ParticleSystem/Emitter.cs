7bc7130f9fcb4af44d9e874d8d8bc0882ba35538
Jimmy Sihlberg <tfy12jsg@cs.umu.se>
2016-04-11 23:05:07 +0200

    Render particles with unitys particle system worksgit statusgit status

--- Assets/Scipts/Objects/ParticleSystem/Emitter.cs
+++ Assets/Scipts/Objects/ParticleSystem/Emitter.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-using PE;
-
 public class Emitter : MonoBehaviour {
 
     public float rate = 1.0f;
 
+    public int resolution = 10;
+    private ParticleSystem.Particle[] points;
     //public GameObject particlePrefab;
 
     private PE.ParticleSystem ps;
@@ -16,8 +16,22 @@ public class Emitter : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         ps = new PE.ParticleSystem();
-        Engine.instance.addParticleSystem(ps);
-	}
+        PE.Engine.instance.addParticleSystem(ps);
+
+        points = new ParticleSystem.Particle[resolution*8];
+        float increment = 1f / (resolution - 1);
+        for (int y = 0; y < 8; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                float p = x * increment;
+                points[y*resolution + x].position = new Vector3(p, 0.2f*y, 0f);
+                points[y*resolution + x].startColor = new Color(x, 0f, 0f);
+                points[y*resolution + x].startSize = 0.1f;
+            }
+            
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -31,11 +45,13 @@ public class Emitter : MonoBehaviour {
             PE.Particle p = new PE.Particle()
             {
                 x = PE.UnityAdapter.PEVector(pos),
-                v = new Vec3(0.1, 0, 0),
-                f = new Vec3(0),
+                v = new PE.Vec3(0.1, 0, 0),
+                f = new PE.Vec3(0),
                 m = 0.0,
                 age = 0.0
             };
+
+            GetComponent<ParticleSystem>().SetParticles(points, points.Length);
 
             ps.AddParticle(p); 
 
