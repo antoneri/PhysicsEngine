62c2e0a2d96a3c0afd29a9e4e42342ec2be25e2a
Jimmy Sihlberg <tfy12jsg@cs.umu.se>
2016-04-11 09:50:07 +0200

    init project

---
+++ Assets/Engine.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Engine : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(GameLoop());
+
+    }
+	
+	private IEnumerator GameLoop()
+    {
+        /* Simulation loop */
+        while (true)
+        {
+
+            /*Create particles at emitter
+            (Remove particles at sinks or when they expire in time) */
+
+            /*Do inter-particle collision detection and construct a
+            neighbour list – or use a fixed interaction list (cloth). */
+
+            
+            /*Loop over neighbour lists and compute interaction
+            forces.Accumulate the forces.Use Newton’s third law. */
+
+            /*Accumulate external forces from e.g.gravity.*/
+
+            /*Accumulate dissipative forces, e.g.drag and viscous drag. */
+
+            /* Find contact sets with external boundaries, e.g.a plane.
+            Handle external boundary conditions by reflecting the
+            the velocities. */
+
+            /*Take a timestep and integrate using e.g.Verlet / Leap Frog */
+
+             yield return new WaitForFixedUpdate();
+        }
+    }
+}
+
+
+
---
+++ Assets/cameraController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour {
+
+    private Vector3 lastMouse;
+
+    public float speed = 0.1f;
+    public float sensitivity = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMouse = Input.mousePosition;
+        }
+
+		if (Input.GetMouseButton(0)) {
+            Vector3 delta = Input.mousePosition - lastMouse;
+
+            delta *= sensitivity;
+            Vector3 rot = new Vector3(transform.eulerAngles.x - delta.y, transform.eulerAngles.y + delta.x, 0);
+            transform.eulerAngles = rot;
+
+            lastMouse = Input.mousePosition;
+
+        }
+
+		float forward = Input.GetAxis ("Vertical");
+		float side = Input.GetAxis ("Horizontal");
+        float up = 0f;
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            up = 1.0f;
+        } else if (Input.GetKey(KeyCode.Q))
+        {
+            up = -1.0f;
+        }
+
+        Vector3 dir = new Vector3(side, up, forward);
+        dir.Normalize();
+
+        Vector3 movement = dir * speed * Time.deltaTime;
+
+		transform.Translate (movement);
+
+	}
+
+}
