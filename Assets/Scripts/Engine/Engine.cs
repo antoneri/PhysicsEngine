e09ded72e2fdb14e4d0e38fd04d08121afc92993
Anton Eriksson <antoneri@gmail.com>
2016-04-17 18:28:38 +0200

    Added Stokes drag force

--- Assets/Scripts/Engine/Engine.cs
+++ Assets/Scripts/Engine/Engine.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -8,7 +9,8 @@ namespace PE
 	{
 		public static Engine instance = null;
 
-		readonly Vec3 g = new Vec3 (0, -9.82, 0);
+		private readonly Vec3 g = new Vec3 (0, -9.82, 0);
+
 		List<ParticleSystem> particleSystems = new List<ParticleSystem> ();
 
 		void Awake ()
@@ -40,38 +42,36 @@ namespace PE
 			while (true) {
 				double dt = Time.fixedDeltaTime;
 
-				/*Create particles at emitter
-            	(Remove particles at sinks or when they expire in time) */
 				foreach (var particleSystem in particleSystems) {
+					// Create particles at emitter
+					// (Remove particles at sinks or when they expire in time)
 					particleSystem.Update ();
-				}
 
-				/*Do inter-particle collision detection and construct a
-				neighbour list – or use a fixed interaction list (cloth). */
+					// Do inter-particle collision detection and construct a
+					// neighbour list – or use a fixed interaction list (cloth).
 
-				/*Loop over neighbour lists and compute interaction
-				forces.Accumulate the forces.Use Newton’s third law. */
+					// Loop over neighbour lists and compute interaction forces.
+					// Accumulate the forces.Use Newton’s third law.
 
-				/*Accumulate external forces from e.g.gravity.*/
-				foreach (var particleSystem in particleSystems) {
+					// Accumulate external forces from e.g.gravity.
 					foreach (var p in particleSystem) {
 						p.f = p.m * g;
+						p.f -= 0.05 * p.m * p.v;
 					}
-				}
 
-				/*Accumulate dissipative forces, e.g.drag and viscous drag. */
-					
-				/* Find contact sets with external boundaries, e.g.a plane.
-				Handle external boundary conditions by reflecting the
-				the velocities. */
+					// Accumulate dissipative forces, e.g.drag and viscous drag.
+
+					// Find contact sets with external boundaries, e.g.a plane.
+					// Handle external boundary conditions by reflecting the
+					// the velocities.
 
 
-				/*Take a timestep and integrate using e.g.Verlet / Leap Frog */
-				foreach (var particleSystem in particleSystems) {
+					// Take a timestep and integrate using e.g.Verlet / Leap Frog
 					foreach (var p in particleSystem) {
-						p.v = p.v + dt * p.f / p.m;
+						p.v = p.v + dt * p.m_inv * p.f;
 						p.x = p.x + dt * p.v;
 					}
+
 				}
 
 				yield return new WaitForFixedUpdate ();
