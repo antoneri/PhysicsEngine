1fdf241260e0106c564466aed8188c60d2f7be4c
Anton Eriksson <antoneri@gmail.com>
2016-04-21 17:12:58 +0200

    Sort of not working integration of Cloth

--- Assets/Scripts/Engine/Engine.cs
+++ Assets/Scripts/Engine/Engine.cs
@@ -15,9 +15,9 @@ namespace PE
 		List<Entity> entities = new List<Entity> ();
 		List<ParticleMesh> particleMeshes = new List<ParticleMesh> ();
 
-        private List<IntersectData> intersections = new List<IntersectData>(10000);
+		private List<IntersectData> intersections = new List<IntersectData> (10000);
 
-        void Awake ()
+		void Awake ()
 		{
 			if (instance == null)
 				instance = this;
@@ -77,16 +77,15 @@ namespace PE
 						p.f.z -= 0.05 * p.m * p.v.z;
 					}
 
-                    // Accumulate dissipative forces, e.g.drag and viscous drag.
+					// Accumulate dissipative forces, e.g.drag and viscous drag.
 
-                    // Find contact sets with external boundaries, e.g.a plane.
-                    // Handle external boundary conditions by reflecting the
-                    // the velocities.
-                    intersections.Clear();
-                    foreach (Entity e in entities)
-                    {
-                        intersections.AddRange(e.getCollider().Collides(particleSystem));
-                    }
+					// Find contact sets with external boundaries, e.g.a plane.
+					// Handle external boundary conditions by reflecting the
+					// the velocities.
+					intersections.Clear ();
+					foreach (Entity e in entities) {
+						intersections.AddRange (e.getCollider ().Collides (particleSystem));
+					}
                     
 					foreach (IntersectData data in intersections) {
 						double e = 0.8;
@@ -123,32 +122,39 @@ namespace PE
 				}
 
 				foreach (var particles in particleMeshes) {
+					// Reset forces
 					for (int i = 0; i < particles.Size; i++) {
 						var p = particles [i];
 						p.f.x = p.f.y = p.f.z = 0;
 					}
 
 					foreach (var n in particles.Neighbors) {
-						var p1 = n.p1;
-						var p2 = n.p2;
-						var r = p1.x - p2.x;
-						var v = p1.v - p2.v;
-						p2.f += -(n.k * (r.Length - n.x0) + n.kd * Vec3.Dot (v, r) / r.Length) * r.Normalize;
-						p1.f += -p2.f;
+						var pa = n.p1;
+						var pb = n.p2;
+						var r = pa.x - pb.x;
+						var v = pa.v - pb.v;
+						pb.f += -(n.k * (r.Length - n.x0) + n.kd * Vec3.Dot (v, r) / r.Length) * r.Normalize;
+						pa.f += -pb.f;
 					}
 
-					for (int i = 0; i < particles.Rows; i++) {
-						for (int j = 0; j < particles.Cols; j++) {
-							var p = particles [i, j];
-							if (j == 0 && (i == 0 || i == particles.Rows - 1)) {
-								// Top corners
-								continue;
-							}
-
-							p.f += p.m * g;
-							p.v += dt * p.m_inv * p.f;
-							p.x += dt * p.v;
+					for (int i = 0; i < particles.Size; i++) {
+						var p = particles [i];
+						if (i == 0 || i == particles.Rows - 1) {
+							// Top corners
+							// TODO Add upwards force instead
+							continue;
 						}
+
+						// Add gravity
+						p.f += p.m * g;
+
+						// Integrate
+						p.v.x += dt * p.m_inv * p.f.x;
+						p.v.y += dt * p.m_inv * p.f.y;
+						p.v.z += dt * p.m_inv * p.f.z;
+						p.x.x += dt * p.v.x;
+						p.x.y += dt * p.v.y;
+						p.x.z += dt * p.v.z;
 					}
 				}
 
