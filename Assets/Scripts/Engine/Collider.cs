ac9b12d717f207c416eb578fb9abe6fbe9026f3d
Anton Eriksson <antoneri@gmail.com>
2016-04-25 11:59:21 +0200

    Code cleanup. Renamed IntersectData -> Intersection. Extracted ParticleUpdate from GameLoop.

--- Assets/Scripts/Engine/Collider.cs
+++ Assets/Scripts/Engine/Collider.cs
@@ -5,155 +5,158 @@ using System;
 
 using PE;
 
-public struct IntersectData {
-    public PE.Particle particle;
-    public double distance;
-    public Vec3 normal;
-    public Vec3 point;
+public struct Intersection
+{
+	public PE.Particle particle;
+	public double distance;
+	public Vec3 normal;
+	public Vec3 point;
 
-    public override string ToString()
-    {
-        return distance + "\n" + normal.ToString() + "\n" + point.ToString();
-    }
+	public override string ToString ()
+	{
+		return distance + "\n" + normal.ToString () + "\n" + point.ToString ();
+	}
 }
 
 public abstract class Collider
 {
-    public abstract List<IntersectData> Collides(PE.ParticleSystem ps);
+	public abstract List<Intersection> Collides (PE.ParticleSystem ps);
 
-    public static List<IntersectData> Collides(PE.ParticleSystem ps, Plane pl)
-    {
-        List<IntersectData> intersections = new List<IntersectData>();
-        foreach (var p in ps)
-        {
-            double d = Math.Abs(Vec3.Dot(p.x, pl.normal) - pl.d);
-            if (d < Plane.thickness) {
-                IntersectData data = new IntersectData
-                {
-                    particle = p,
-                    distance = Plane.thickness - d,
-                    normal = pl.normal,
-                    point = (p.x - (Vec3.Dot(pl.normal, p.x) + pl.d) * pl.normal) + Plane.thickness * pl.normal
-                };
-                intersections.Add(data);
-            }
-        }
-        return intersections;
-    }
+	public static List<Intersection> Collides (PE.ParticleSystem ps, Plane pl)
+	{
+		List<Intersection> intersections = new List<Intersection> ();
+		foreach (var p in ps) {
+			double d = Math.Abs (Vec3.Dot (p.x, pl.normal) - pl.d);
+			if (d < Plane.thickness) {
+				var data = new Intersection {
+					particle = p,
+					distance = Plane.thickness - d,
+					normal = pl.normal,
+					point = (p.x - (Vec3.Dot (pl.normal, p.x) + pl.d) * pl.normal) + Plane.thickness * pl.normal
+				};
+				intersections.Add (data);
+			}
+		}
+		return intersections;
+	}
 
-    public static List<IntersectData> Collides(PE.ParticleSystem ps, AABB b)
-    {
-        List<IntersectData> intersections = new List<IntersectData>();
-        foreach (var p in ps)
-        {
-            if (p.x.x < b.max.x && p.x.x > b.min.x &&
-                p.x.y < b.max.y && p.x.y > b.min.y &&
-                p.x.z < b.max.z && p.x.z > b.min.z) {
+	public static List<Intersection> Collides (PE.ParticleSystem ps, AABB b)
+	{
+		List<Intersection> intersections = new List<Intersection> ();
+		foreach (var p in ps) {
+			if (p.x.x < b.max.x && p.x.x > b.min.x &&
+			    p.x.y < b.max.y && p.x.y > b.min.y &&
+			    p.x.z < b.max.z && p.x.z > b.min.z) {
 
-                Vec3 n = PointNormalAABB(p.x, b);
-                Vec3 AABBPoint = ClosestPointAABB(p.x, b, n);
-                IntersectData data = new IntersectData
-                {
-                    particle = p,
-                    distance = 0,
-                    normal = n,
-                    point = AABBPoint
-                };
-                intersections.Add(data);
-            }
-        }
-        return intersections;
-    }
+				Vec3 n = PointNormalAABB (p.x, b);
+				Vec3 AABBPoint = ClosestPointAABB (p.x, b, n);
+				var data = new Intersection {
+					particle = p,
+					distance = 0,
+					normal = n,
+					point = AABBPoint
+				};
+				intersections.Add (data);
+			}
+		}
+		return intersections;
+	}
 
+	/* AABB intersect data */
+	public static Vec3 PointNormalAABB (Vec3 p, AABB b)
+	{
+		Vec3 normal = new Vec3 ();
+		double minDistance = double.MaxValue;
 
+		if (Math.Abs (b.min.x - p.x) < minDistance) {
+			normal.Set (-1, 0, 0);
+			minDistance = Math.Abs (b.min.x - p.x);
+		}
+		if (Math.Abs (b.max.x - p.x) < minDistance) {
+			normal.Set (1, 0, 0);
+			minDistance = Math.Abs (b.max.x - p.x);
+		}
+		if (Math.Abs (b.min.y - p.y) < minDistance) {
+			normal.Set (0, -1, 0);
+			minDistance = Math.Abs (b.min.y - p.y);
+		}
+		if (Math.Abs (b.max.y - p.y) < minDistance) {
+			normal.Set (0, 1, 0);
+			minDistance = Math.Abs (b.max.y - p.y);
+		}
+		if (Math.Abs (b.min.z - p.z) < minDistance) {
+			normal.Set (0, 0, -1);
+			minDistance = Math.Abs (b.min.z - p.z);
+		}
+		if (Math.Abs (b.max.z - p.z) < minDistance) {
+			normal.Set (0, 0, 1);
+			minDistance = Math.Abs (b.max.z - p.z);
+		}
 
+		return normal;
+	}
 
+	public static Vec3 ClosestPointAABB (Vec3 p, AABB b, Vec3 normal)
+	{
+		Vec3 proj = new Vec3 ();
 
+		if (normal.x + normal.y + normal.z > 0) {
+			proj = p - Vec3.Dot (p - b.max, normal) * normal;
+		} else {
+			proj = p - Vec3.Dot (p - b.min, normal) * normal;
+		}
 
+		return proj;
+	}
 
-
-
-    /* AABB intersect data */
-
-    public static Vec3 PointNormalAABB(Vec3 p, AABB b)
-    {
-        Vec3 normal = new Vec3();
-        double minDistance = double.MaxValue;
-
-        if (Math.Abs(b.min.x - p.x) < minDistance) { normal.Set(-1, 0, 0); minDistance = Math.Abs(b.min.x - p.x); }
-        if (Math.Abs(b.max.x - p.x) < minDistance) { normal.Set(1, 0, 0); minDistance = Math.Abs(b.max.x - p.x); }
-        if (Math.Abs(b.min.y - p.y) < minDistance) { normal.Set(0, -1, 0); minDistance = Math.Abs(b.min.y - p.y); }
-        if (Math.Abs(b.max.y - p.y) < minDistance) { normal.Set(0, 1, 0); minDistance = Math.Abs(b.max.y - p.y); }
-        if (Math.Abs(b.min.z - p.z) < minDistance) { normal.Set(0, 0, -1); minDistance = Math.Abs(b.min.z - p.z); }
-        if (Math.Abs(b.max.z - p.z) < minDistance) { normal.Set(0, 0, 1); minDistance = Math.Abs(b.max.z - p.z); }
-
-        return normal;
-    }
-
-    public static Vec3 ClosestPointAABB(Vec3 p, AABB b, Vec3 normal)
-    {
-        Vec3 proj = new Vec3();
-
-        if (normal.x + normal.y + normal.z > 0)
-        {
-            proj = p - Vec3.Dot(p - b.max, normal) * normal;
-        } else
-        {
-            proj = p - Vec3.Dot(p - b.min, normal) * normal;
-        }
-
-        return proj;
-    }
-
-    public static double SqDistPointAABB(Vec3 p, AABB b)
-    {
-        double sqDist = 0.0f;
-        for (int i = 0; i < 3; i++)
-        {
-            // For each axis count any excess distance outside box extents
-            double v = p[i];
-            if (v < b.min[i]) sqDist += (b.min[i] - v) * (b.min[i] - v);
-            if (v > b.max[i]) sqDist += (v - b.max[i]) * (v - b.max[i]);
-        }
-        return sqDist;
-    }
+	public static double SqDistPointAABB (Vec3 p, AABB b)
+	{
+		double sqDist = 0.0f;
+		for (int i = 0; i < 3; i++) {
+			// For each axis count any excess distance outside box extents
+			double v = p [i];
+			if (v < b.min [i])
+				sqDist += (b.min [i] - v) * (b.min [i] - v);
+			if (v > b.max [i])
+				sqDist += (v - b.max [i]) * (v - b.max [i]);
+		}
+		return sqDist;
+	}
 
 }
 
 public class Plane : Collider
 {
+	public Vec3 normal;
+	public double d;
 
-    public Vec3 normal;
-    public double d;
+	public const double thickness = 0.3;
 
-    public const double thickness = 0.3;
+	public Plane (Vec3 normal, double d)
+	{
+		this.normal = normal;
+		this.d = d;
+	}
 
-    public Plane(Vec3 normal, double d)
-    {
-        this.normal = normal;
-        this.d = d;
-    }
-
-    public override List<IntersectData> Collides(PE.ParticleSystem ps)
-    {
-        return Collides(ps, this);
-    }
-
+	public override List<Intersection> Collides (PE.ParticleSystem ps)
+	{
+		return Collides (ps, this);
+	}
 }
 
 public class AABB : Collider
 {
-    public Vec3 min;
-    public Vec3 max;
+	public Vec3 min;
+	public Vec3 max;
 
-    public AABB(Vec3 min, Vec3 max)
-    {
-        this.min = min;
-        this.max = max;
-    }
+	public AABB (Vec3 min, Vec3 max)
+	{
+		this.min = min;
+		this.max = max;
+	}
 
-    public override List<IntersectData> Collides(PE.ParticleSystem ps)
-    {
-        return Collides(ps, this);
-    }
+	public override List<Intersection> Collides (PE.ParticleSystem ps)
+	{
+		return Collides (ps, this);
+	}
 }
--- Assets/Scripts/Engine/Engine.cs
+++ Assets/Scripts/Engine/Engine.cs
@@ -9,21 +9,24 @@ namespace PE
 	{
 		public static Engine instance = null;
 
-		public Vector3 Wind = new Vector3 (0, 0, 0);
-		private Vec3 wind = new Vec3 (0);
-
-		private const double AIR_P = 1.18;
-		/* Density */
-		private const double AIR_u = 1.002;
-		/* Viscosity */
+		private const float clothFrameRate = 400f;
+		private int clothTimeSteps;
+		private double clothDeltaTime;
 
 		private readonly Vec3 g = new Vec3 (0, -9.82, 0);
 
-		List<ParticleSystem> particleSystems = new List<ParticleSystem> ();
-		List<Entity> entities = new List<Entity> ();
-		List<ParticleMesh> particleMeshes = new List<ParticleMesh> ();
+		public Vector3 Wind = new Vector3 (0, 0, 0);
+		private Vec3 wind = new Vec3 (0);
 
-		private List<IntersectData> intersections = new List<IntersectData> (10000);
+		/* Density */
+		private const double AIR_P = 1.18;
+		/* Viscosity */
+		private const double AIR_u = 1.002;
+
+		private List<ParticleSystem> particleSystems = new List<ParticleSystem> ();
+		private List<Entity> entities = new List<Entity> ();
+		private List<ParticleMesh> particleMeshes = new List<ParticleMesh> ();
+		private List<Intersection> intersections = new List<Intersection> (10000);
 
 		void Awake ()
 		{
@@ -41,9 +44,9 @@ namespace PE
 			particleSystems.Add (ps);
 		}
 
-		public void AddParticleMesh (ParticleMesh particleMesh)
+		public void AddParticleMesh (ParticleMesh mesh)
 		{
-			particleMeshes.Add (particleMesh);
+			particleMeshes.Add (mesh);
 		}
 
 		public void AddEntity (Entity e)
@@ -54,22 +57,20 @@ namespace PE
 		// Use this for initialization
 		void Start ()
 		{
-			StartCoroutine (GameLoop ());
+			clothTimeSteps = Mathf.CeilToInt (Time.fixedDeltaTime * clothFrameRate);
+			clothDeltaTime = Time.fixedDeltaTime / clothTimeSteps;
 		}
 
 		public void FixedUpdate ()
 		{
-			float rate = 400f; // Hz
-			float dT = Time.fixedDeltaTime;
-			int numTimeSteps = Mathf.CeilToInt (dT * rate);
-			var dt = dT / numTimeSteps;
-
-			for (int i = 0; i < numTimeSteps; i++) {
-				ClothUpdate (dt);
+			for (int i = 0; i < clothTimeSteps; i++) {
+				ClothUpdate (clothDeltaTime);
 			}
+
+			ParticleUpdate (Time.fixedDeltaTime);
 		}
 
-		public void ClothUpdate (float dt)
+		public void ClothUpdate (double dt)
 		{
 			foreach (var particles in particleMeshes) {
 				// Reset forces
@@ -108,94 +109,80 @@ namespace PE
 			}
 		}
 
-		private IEnumerator GameLoop ()
+		private void ParticleUpdate (double dt)
 		{
-			/* Simulation loop */
-			while (true) {
-				double dt = Time.fixedDeltaTime;
-				wind = Wind;
+			wind = Wind;
 
-				foreach (var particleSystem in particleSystems) {
+			foreach (var particleSystem in particleSystems) {
 
-					/* Clear forces */
-					foreach (var p in particleSystem) {
-						p.f.Set (0);
-					}
+				/* Clear forces */
+				foreach (var p in particleSystem) {
+					p.f.SetZero ();
+				}
 
-					// Create particles at emitter
-					// (Remove particles at sinks or when they expire in time)
-					particleSystem.Update ();
+				// Create particles at emitter
+				// (Remove particles at sinks or when they expire in time)
+				particleSystem.Update ();
 
-					// Do inter-particle collision detection and construct a
-					// neighbour list – or use a fixed interaction list (cloth).
+				// Do inter-particle collision detection and construct a
+				// neighbour list – or use a fixed interaction list (cloth).
 
-					// Loop over neighbour lists and compute interaction forces.
-					// Accumulate the forces.Use Newton’s third law.
+				// Loop over neighbour lists and compute interaction forces.
+				// Accumulate the forces.Use Newton’s third law.
 
-					// Accumulate external forces from e.g.gravity.
-					// Accumulate dissipative forces, e.g.drag and viscous drag.
-					foreach (var p in particleSystem) {
-						/* Gravity, uses Buoyant force to counter gravity force */
-						double dp = p.p - AIR_P;
-						/* Fb = (Pair - Pp) * g * V */
-						p.f.Add (dp * g * (p.m / p.p));
-						//p.f.Add (p.m * g);
-                        
-
-                        
-						//p.f.Add (-0.5 * p.m * g);
-
-						/* Air drag force */
-						//double kd = 0.000018;
-						Vec3 u = p.v - wind;
-
-						//Vec3 Fair = -kd * u;
-						double C = 0.5; /* Drag constant */
-						/* Air drag: Fdrag = 1/2 * C * P_air * A * V^2 */
-						//Vec3 Fair = -0.5 * C * AIR_P * p.r * p.r * Math.PI * p.v.SqLength * p.v.UnitVector;
-						Vec3 Fair = -6 * Math.PI * AIR_u * p.r * u;
-
-						p.f.Add (Fair);
-					}
-						
-					// Find contact sets with external boundaries, e.g.a plane.
-					// Handle external boundary conditions by reflecting the
-					// the velocities.
-					intersections.Clear ();
-					foreach (Entity e in entities) {
-						intersections.AddRange (e.Collider.Collides (particleSystem));
-					}
+				// Accumulate external forces from e.g.gravity.
+				// Accumulate dissipative forces, e.g.drag and viscous drag.
+				foreach (var p in particleSystem) {
+					/* Gravity, uses Buoyant force to counter gravity force */
+					//double dp = p.p - AIR_P;
+					/* Fb = (Pair - Pp) * g * V */
+					//p.f.Add (dp * g * (p.m / p.p));
+					p.f.Add (p.m * g);
                     
-					foreach (IntersectData data in intersections) {
-						double e = 0.8;
-						Vec3 v = data.particle.v;
-						data.particle.v = v - (1 + e) * (Vec3.Dot (v, data.normal) * data.normal);
-					}
-                    
+					/* Air drag force */
+					//double kd = 0.000018;
+					Vec3 u = p.v - wind;
 
-
-					// Take a timestep and integrate using e.g.Verlet / Leap Frog
-					foreach (var p in particleSystem) {
-						p.v.Add (dt * p.m_inv * p.f);
-
-						p.x.Add (dt * p.v);
-					}
-
-					/* Adjust collided particles */
-					foreach (IntersectData data in intersections) {
-						Particle p = data.particle;
-						if (Vec3.Dot (p.v, data.normal) < 0) {
-							p.v.Set (0.0);
-						}
-						double x = Vec3.Dot (p.x - data.point, data.normal);
-						if (x < 0) {
-							p.x = data.point;
-						}
-					}
-
+					//Vec3 Fair = -kd * u;
+					double C = 0.5; /* Drag constant */
+					/* Air drag: Fdrag = 1/2 * C * P_air * A * V^2 */
+					//Vec3 Fair = -0.5 * C * AIR_P * p.r * p.r * Math.PI * p.v.SqLength * p.v.UnitVector;
+					Vec3 Fair = -6 * Math.PI * AIR_u * p.r * u;
+					//p.f.Add (Fair);
 				}
 					
-				yield return new WaitForFixedUpdate ();
+				// Find contact sets with external boundaries, e.g.a plane.
+				// Handle external boundary conditions by reflecting the
+				// the velocities.
+				intersections.Clear ();
+
+				foreach (var entity in entities) {
+					intersections.AddRange (entity.Collider.Collides (particleSystem));
+				}
+                
+				foreach (var data in intersections) {
+					double e = 0.8;
+					var v = data.particle.v;
+					data.particle.v = v - (1 + e) * Vec3.Dot (v, data.normal) * data.normal;
+				}
+
+				// Take a timestep and integrate using e.g.Verlet / Leap Frog
+				foreach (var p in particleSystem) {
+					p.v.Add (dt * p.m_inv * p.f);
+					p.x.Add (dt * p.v);
+				}
+
+				/* Adjust collided particles */
+				foreach (var data in intersections) {
+					var p = data.particle;
+					if (Vec3.Dot (p.v, data.normal) < 0) {
+						p.v.SetZero ();
+					}
+
+					if (Vec3.Dot (p.x - data.point, data.normal) < 0) {
+						p.x = data.point;
+					}
+				}
 			}
 		}
 
