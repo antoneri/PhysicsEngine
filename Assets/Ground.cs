85248900f5ea4aaaeac8cc223a8d02d1c7224d85
Anton Eriksson <antoneri@gmail.com>
2016-04-25 11:20:47 +0200

    Refactored Entity to use class properties instead of getters and setters.

--- Assets/Ground.cs
+++ Assets/Ground.cs
@@ -3,20 +3,22 @@ using System.Collections;
 
 public class Ground : MonoBehaviour
 {
-    private Entity ground;
+	private Entity ground;
 
 	// Use this for initialization
-	void Start () {
-        ground = new Entity();
-        double d = transform.position.magnitude;
-        ground.setCollider(new Plane(new PE.Vec3(0, 1.0, 0), d));
-        ground.setStaticObject(true);
+	void Start ()
+	{
+		ground = new Entity ();
+		double d = transform.position.magnitude;
+		ground.Collider = new Plane (new PE.Vec3 (0, 1.0, 0), d);
+		ground.Static = true;
 
-        PE.Engine.instance.AddEntity(ground);
+		PE.Engine.instance.AddEntity (ground);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 }
