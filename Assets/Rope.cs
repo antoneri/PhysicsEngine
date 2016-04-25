3b32cb57af71febf7bfc1fa05c52c0530367d5cb
Jimmy Sihlberg <tfy12jsg@cs.umu.se>
2016-04-25 16:27:18 +0200

    created Rope script

---
+++ Assets/Rope.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PE {
+    public class Rope : MonoBehaviour
+    {
+
+        ParticleMesh particles;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
+
