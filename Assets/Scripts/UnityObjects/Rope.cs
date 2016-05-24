c752548ba023671d4f974eef6a60c6e8ec66ff10
Anton Eriksson <tfy12aen@cs.umu.se>
2016-05-24 13:38:25 +0200

    Switched Rope to LineRenderer.

--- Assets/Scripts/UnityObjects/Rope.cs
+++ Assets/Scripts/UnityObjects/Rope.cs
@@ -8,18 +8,23 @@ namespace PE
 	{
 		public int NUM_PARTICLES = 20;
 
-        public double mass_last_particle = 1;
-        public double spring_k = 1000;
+		public double mass_last_particle = 1;
+		public double spring_k = 1000;
 
 		ParticleSystem particles = new ParticleSystem ();
-        private UnityEngine.ParticleSystem.Particle[] unityParticles;
+
+		private Vector3[] lineVertices;
+		private LineRenderer lineRenderer;
 
 		// Use this for initialization
 		void Start ()
 		{
-            unityParticles = new UnityEngine.ParticleSystem.Particle[NUM_PARTICLES];
+			lineVertices = new Vector3[NUM_PARTICLES];
+			lineRenderer = GetComponent<UnityEngine.LineRenderer> ();
+			lineRenderer.SetVertexCount (NUM_PARTICLES);
+			lineRenderer.SetWidth (0.1f, 0.1f);
 
-            var startPos = new Vec3 (-3, 15, 0);
+			var startPos = new Vec3 (-3, 15, 0);
 			var dx = new Vec3 (-0.25, -0.25, 0);
 			var mass = 1.0;
 
@@ -27,34 +32,29 @@ namespace PE
 				particles.Add (new Particle (startPos + i * dx, mass));
 			}
 
-            particles[NUM_PARTICLES - 1].m = mass_last_particle;
+			particles [NUM_PARTICLES - 1].m = mass_last_particle;
 
-            List<Constraint> constraints = new List<Constraint>();
-            constraints.Add(new PositionConstraint(0, 0, startPos));
-            double length = 0.1;
-            for (int i = 0; i < NUM_PARTICLES - 1; i++)
-            {
-                constraints.Add(new DistanceConstraint(i, i + 1, length, spring_k));
-            }
+			List<Constraint> constraints = new List<Constraint> ();
+			constraints.Add (new PositionConstraint (0, 0, startPos));
 
-            particles.constraints = constraints;
+			double length = 0.1;
+			for (int i = 0; i < NUM_PARTICLES - 1; i++) {
+				constraints.Add (new DistanceConstraint (i, i + 1, length, spring_k));
+			}
 
-            Engine.instance.AddRope (particles);
+			particles.constraints = constraints;
+
+			Engine.instance.AddRope (particles);
 		}
 
 		// Update is called once per frame
 		void Update ()
 		{
-			for (int i = 0; i < particles.Count; i++) {
-				unityParticles [i] = new UnityEngine.ParticleSystem.Particle () {
-					position = (Vector3)particles [i].x - transform.position,
-					startColor = Color.black,
-					startSize = 0.5f,
-				};
+			for (int i = 0; i < NUM_PARTICLES; i++) {
+				lineVertices [i] = particles [i].x;
 			}
 
-			GetComponent<UnityEngine.ParticleSystem> ().SetParticles (unityParticles, particles.Count);
-
+			lineRenderer.SetPositions (lineVertices);
 		}
 	}
 
