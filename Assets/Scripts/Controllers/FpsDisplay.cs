8b1f5ac2a0e18acb9dbb0259d7e36a91aa7a7daf
Anton Eriksson <tfy12aen@cs.umu.se>
2016-05-31 16:45:53 +0200

    Add Fps Counter

---
+++ Assets/Scripts/Controllers/FpsDisplay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class FpsDisplay : MonoBehaviour
+{
+    float deltaTime = 0.0f;
+    GUIStyle style = new GUIStyle ();
+
+    void Start () {
+        style.alignment = TextAnchor.UpperLeft;
+        style.normal.textColor = new Color (1.0f, 1.0f, 1.0f, 1.0f);
+    }
+
+    void Update () {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+    }
+
+    void OnGUI () {
+        int w = Screen.width;
+        int h = Screen.height;
+
+        Rect rect = new Rect (0, 0, w, h * 2 / 100);
+        style.fontSize = h * 2 / 100;
+
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        string text = string.Format ("{0:0.0} ms ({1:0.} fps)", msec, fps);
+
+        GUI.Label (rect, text, style);
+    }
+}
